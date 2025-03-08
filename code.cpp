c86fe1bb223a5450d85c9ef0473334e5d598ab55
Twuhed Ahmed Rifat <twuhed806@gmail.com>
2025-03-08 11:22:04 +0600

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -17,7 +17,23 @@ private:
 public:
     TrafficSignal(int green, int red, int pedestrian)
         : greenTime(green), redTime(red), pedestrianTime(pedestrian), trafficDensity(0) {}
-
+ void adjustSignal(int density) {
+        trafficDensity = density;
+        if (trafficDensity > 50) {
+            greenTime += 10;
+            redTime -= 10;
+        } else {
+            greenTime = 30;
+            redTime = 30;
+        }
+    }
    int main(){
+        TrafficSignal signal(30, 30, 20);
+
+    int trafficDensity;
+    cout << "Enter traffic density (0-100): ";
+    cin >> trafficDensity;
+
+    signal.adjustSignal(trafficDensity);
        return 0;
    }            
